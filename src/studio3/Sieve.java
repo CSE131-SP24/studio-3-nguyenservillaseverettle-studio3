67bd473b05eddbe47f6832e163e9a88b4b550ede
skylerlion <skylerlion@10.232.186.117>
2024-02-07 13:29:42 -0600

    h

---
+++ src/studio3/Sieve.java
@@ -0,0 +1,19 @@
+package studio3;
+
+import java.util.Scanner;
+
+public class Sieve {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Find prime numbers up to what number?");
+		int n = in.nextInt();
+		int [][] primes = new int[n][];
+		while (primes < n) {
+			
+		}
+		
+
+	}
+
+}
