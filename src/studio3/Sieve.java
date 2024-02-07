b713cca4be5702ccbb087e05f193e40f6087ca2c
a-c-everett <a.c.everett@wustl.edu>
2024-02-07 14:16:37 -0600

    Done

--- src/studio3/Sieve.java
+++ src/studio3/Sieve.java
@@ -8,9 +8,19 @@ public class Sieve {
 		Scanner in = new Scanner(System.in);
 		System.out.println("Find prime numbers up to what number?");
 		int n = in.nextInt();
-		int [][] primes = new int[n][];
-		while (primes < n) {
-			
+		int[] primes = new int[n-2];
+		
+		for(int i=2; i<n; i++) {
+			for(int j=(2*i); j<n; j=j+i) {
+				primes[(j-2)]=1;
+				
+			}
+		}
+		System.out.print("The prime numbers up to " + n + " are:");
+		for(int i=0; i<n-2; i++) {
+			if(primes[i]==0) {
+				System.out.print(" " + (i+2) + " ");
+			}
 		}
 		
 
